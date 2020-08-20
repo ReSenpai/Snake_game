31514abea737496f646006adcd5986aea14b5b7c
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-20 13:30:36 +0300

    Snake app

---
+++ game.js
@@ -0,0 +1,53 @@
+import { 
+    SNAKE_SPEED, 
+    update as updateSnake, 
+    draw as drawSnake, 
+    getSnakeHead,
+    snakeIntersection
+} from './snake.js';
+import { 
+    update as updateFood,
+    draw as drawFood 
+} from './food.js';
+import { outsideGrid } from './grid.js';
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game_board');
+
+const main = (currentTime) => {
+    if (gameOver) {
+        if (confirm('Вы проигали. Нажми ОК для перезапуска')) {
+            window.location = '/';
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+}
+
+window.requestAnimationFrame(main);
+
+const update = () => {
+    updateSnake();
+    updateFood();
+    chekDeath();
+}
+
+const draw = () => {
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+const chekDeath = () => {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
+
